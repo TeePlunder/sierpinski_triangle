106bc7efe0b02a3d52049063867eb3dfab0ab57c
TeePlunder <leon.bergmann23@gmail.com>
2023-01-27 09:47:09 +0100

    current status

--- sierpinski_triangle.py
+++ sierpinski_triangle.py
@@ -1,5 +1,27 @@
-import turtle as t
+import turtle as turtle
 
-for i in range(3):
-    t.forward(200)
-    t.left(120)
+# triangle configurations
+triangleEdgeCounter = 3
+triangleWidth = 300
+triangleAngle = 120
+
+
+def drawbasictriangle():
+    for i in range(triangleEdgeCounter):
+        turtle.forward(triangleWidth)
+        turtle.left(triangleAngle)
+
+
+drawbasictriangle()
+
+for i in range(4):
+    division = 2
+    if i > 0:
+        division = division * 2
+    print(division)
+    turtle.left(triangleAngle / 2)
+    turtle.forward(triangleWidth / division)
+    turtle.right(triangleAngle / 2)
+    for j in range(triangleEdgeCounter):
+        turtle.forward(triangleWidth / division)
+        turtle.right(triangleAngle)
