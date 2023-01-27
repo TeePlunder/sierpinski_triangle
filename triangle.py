2954ae45a005729718a90b9835da68a212dd6550
TeePlunder <leon.bergmann23@gmail.com>
2023-01-27 23:02:26 +0100

    add a triangle class

---
+++ triangle.py
@@ -0,0 +1,6 @@
+class Triangle:
+    def __init__(self, drawn_edge, position_a, position_b, position_c):
+        self.drawn_edge = drawn_edge
+        self.position_a = position_a
+        self.position_b = position_b
+        self.position_c = position_c
